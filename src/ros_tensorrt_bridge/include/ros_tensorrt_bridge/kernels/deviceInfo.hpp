be2f3d5427d4ff0f3927c302a9333e1efd0003c7
Fazli Gorkem Bal <fazligorkembal@gmail.com>
2025-05-08 10:44:04 +0300

    Rebase

--- src/ros_tensorrt_bridge/include/ros_tensorrt_bridge/kernels/deviceInfo.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ROS_TENSORRT_BRIDGE__DEVICE_INFO_HPP
-#define ROS_TENSORRT_BRIDGE__DEVICE_INFO_HPP
-
-#include <cuda_runtime.h>
-#include <stdio.h>
-
-void deviceInfo();
-
-#endif // ROS_TENSORRT_BRIDGE__DEVICE_INFO_HPP
---
+++ src/ros_tensorrt_bridge/src/main_test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello from main_test!" << std::endl;
+    return 0;
+}
